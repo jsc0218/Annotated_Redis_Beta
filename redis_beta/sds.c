3720ca7b95b4316ae461baa934a2580cac21d0f3
Shichao Jin <jsc0218@gmail.com>
2014-02-17 22:36:48 +0800

    sds 30%

--- redis_beta/sds.c
+++ redis_beta/sds.c
@@ -38,9 +38,7 @@ static void sdsOomAbort(void) {
 }
 
 sds sdsnewlen(const void *init, size_t initlen) {
-    struct sdshdr *sh;
-
-    sh = malloc(sizeof(struct sdshdr)+initlen+1);
+    struct sdshdr *sh = malloc(sizeof(struct sdshdr) + initlen + 1);
 #ifdef SDS_ABORT_ON_OOM
     if (sh == NULL) sdsOomAbort();
 #else
@@ -50,14 +48,14 @@ sds sdsnewlen(const void *init, size_t initlen) {
     sh->free = 0;
     if (initlen) {
         if (init) memcpy(sh->buf, init, initlen);
-        else memset(sh->buf,0,initlen);
+        else memset(sh->buf, 0, initlen);
     }
     sh->buf[initlen] = '\0';
     return (char*)sh->buf;
 }
 
 sds sdsempty(void) {
-    return sdsnewlen("",0);
+    return sdsnewlen("", 0);
 }
 
 sds sdsnew(const char *init) {
@@ -66,7 +64,7 @@ sds sdsnew(const char *init) {
 }
 
 size_t sdslen(const sds s) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
     return sh->len;
 }
 
@@ -76,52 +74,46 @@ sds sdsdup(const sds s) {
 
 void sdsfree(sds s) {
     if (s == NULL) return;
-    free(s-sizeof(struct sdshdr));
+    free(s - sizeof(struct sdshdr));
 }
 
 size_t sdsavail(sds s) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
     return sh->free;
 }
 
 void sdsupdatelen(sds s) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
     int reallen = strlen(s);
-    sh->free += (sh->len-reallen);
+    sh->free += (sh->len - reallen);
     sh->len = reallen;
 }
 
 static sds sdsMakeRoomFor(sds s, size_t addlen) {
-    struct sdshdr *sh, *newsh;
     size_t free = sdsavail(s);
-    size_t len, newlen;
-
     if (free >= addlen) return s;
-    len = sdslen(s);
-    sh = (void*) (s-(sizeof(struct sdshdr)));
-    newlen = (len+addlen)*2;
-    newsh = realloc(sh, sizeof(struct sdshdr)+newlen+1);
+    size_t len = sdslen(s);
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
+    size_t newlen = (len + addlen) * 2;
+    struct sdshdr *newsh = realloc(sh, sizeof(struct sdshdr) + newlen + 1);
 #ifdef SDS_ABORT_ON_OOM
     if (newsh == NULL) sdsOomAbort();
 #else
     if (newsh == NULL) return NULL;
 #endif
-
     newsh->free = newlen - len;
     return newsh->buf;
 }
 
 sds sdscatlen(sds s, void *t, size_t len) {
-    struct sdshdr *sh;
     size_t curlen = sdslen(s);
-
-    s = sdsMakeRoomFor(s,len);
+    s = sdsMakeRoomFor(s, len);
     if (s == NULL) return NULL;
-    sh = (void*) (s-(sizeof(struct sdshdr)));
-    memcpy(s+curlen, t, len);
-    sh->len = curlen+len;
-    sh->free = sh->free-len;
-    s[curlen+len] = '\0';
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
+    memcpy(s + curlen, t, len);
+    sh->len = curlen + len;
+    sh->free = sh->free - len;
+    s[curlen + len] = '\0';
     return s;
 }
 
@@ -130,19 +122,18 @@ sds sdscat(sds s, char *t) {
 }
 
 sds sdscpylen(sds s, char *t, size_t len) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
-    size_t totlen = sh->free+sh->len;
-
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
+    size_t totlen = sh->free + sh->len;
     if (totlen < len) {
-        s = sdsMakeRoomFor(s,len-totlen);
+        s = sdsMakeRoomFor(s, len-totlen);
         if (s == NULL) return NULL;
-        sh = (void*) (s-(sizeof(struct sdshdr)));
-        totlen = sh->free+sh->len;
+        sh = (void*) (s - sizeof(struct sdshdr));
+        totlen = sh->free + sh->len;
     }
     memcpy(s, t, len);
     s[len] = '\0';
     sh->len = len;
-    sh->free = totlen-len;
+    sh->free = totlen - len;
     return s;
 }
 
@@ -152,11 +143,10 @@ sds sdscpy(sds s, char *t) {
 
 sds sdscatprintf(sds s, const char *fmt, ...) {
     va_list ap;
-    char *buf, *t;
-    size_t buflen = 32;
-
     va_start(ap, fmt);
-    while(1) {
+    char *buf;
+    size_t buflen = 32;
+    while (1) {
         buf = malloc(buflen);
 #ifdef SDS_ABORT_ON_OOM
         if (buf == NULL) sdsOomAbort();
@@ -173,32 +163,28 @@ sds sdscatprintf(sds s, const char *fmt, ...) {
         break;
     }
     va_end(ap);
-    t = sdscat(s, buf);
+    char *t = sdscat(s, buf);
     free(buf);
     return t;
 }
 
 sds sdstrim(sds s, const char *cset) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
     char *start, *end, *sp, *ep;
-    size_t len;
-
     sp = start = s;
-    ep = end = s+sdslen(s)-1;
-    while(sp <= end && strchr(cset, *sp)) sp++;
-    while(ep > start && strchr(cset, *ep)) ep--;
-    len = (sp > ep) ? 0 : ((ep-sp)+1);
+    ep = end = s + sdslen(s) - 1;
+    while (sp <= end && strchr(cset, *sp)) sp++;
+    while (ep > start && strchr(cset, *ep)) ep--;
+    size_t len = (sp > ep) ? 0 : ((ep-sp)+1);
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
     if (sh->buf != sp) memmove(sh->buf, sp, len);
     sh->buf[len] = '\0';
-    sh->free = sh->free+(sh->len-len);
+    sh->free = sh->free + (sh->len - len);
     sh->len = len;
     return s;
 }
 
 sds sdsrange(sds s, long start, long end) {
-    struct sdshdr *sh = (void*) (s-(sizeof(struct sdshdr)));
-    size_t newlen, len = sdslen(s);
-
+    size_t len = sdslen(s);
     if (len == 0) return s;
     if (start < 0) {
         start = len+start;
@@ -208,42 +194,38 @@ sds sdsrange(sds s, long start, long end) {
         end = len+end;
         if (end < 0) end = 0;
     }
-    newlen = (start > end) ? 0 : (end-start)+1;
+    size_t newlen = (start > end) ? 0 : (end-start)+1;
     if (newlen != 0) {
-        if (start >= (signed)len) start = len-1;
-        if (end >= (signed)len) end = len-1;
+        if (start >= (signed)len) start = len - 1;
+        if (end >= (signed)len) end = len - 1;
         newlen = (start > end) ? 0 : (end-start)+1;
     } else {
         start = 0;
     }
+    struct sdshdr *sh = (void*) (s - sizeof(struct sdshdr));
     if (start != 0) memmove(sh->buf, sh->buf+start, newlen);
     sh->buf[newlen] = 0;
-    sh->free = sh->free+(sh->len-newlen);
+    sh->free = sh->free + (sh->len - newlen);
     sh->len = newlen;
     return s;
 }
 
 void sdstolower(sds s) {
     int len = sdslen(s), j;
-
     for (j = 0; j < len; j++) s[j] = tolower(s[j]);
 }
 
 void sdstoupper(sds s) {
     int len = sdslen(s), j;
-
     for (j = 0; j < len; j++) s[j] = toupper(s[j]);
 }
 
 int sdscmp(sds s1, sds s2) {
-    size_t l1, l2, minlen;
-    int cmp;
-
-    l1 = sdslen(s1);
-    l2 = sdslen(s2);
-    minlen = (l1 < l2) ? l1 : l2;
-    cmp = memcmp(s1,s2,minlen);
-    if (cmp == 0) return l1-l2;
+    size_t l1 = sdslen(s1);
+    size_t l2 = sdslen(s2);
+    size_t minlen = (l1 < l2) ? l1 : l2;
+    int cmp = memcmp(s1, s2, minlen);
+    if (cmp == 0) return l1 - l2;
     return cmp;
 }
 
